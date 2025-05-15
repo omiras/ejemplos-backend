d7d0f5e714a32f6ea2227b36809f4cda060e7701
omiras <oscar.miras@gmail.com>
2025-05-15 19:19:56 +0200

    done

--- express/app.js
+++ express/app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// Nos permite procesar peticiones POST que vengan de un formulario
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 // Oye, si te hacen una petición GET, mira si tienes alguno de los recursos que te piden en el directorio 'public'
 app.use(express.static('public'))
 
@@ -18,6 +22,25 @@ app.get('/search', (req, res)=>{
   console.log("Petición recibida");
   res.send(req.query);
 });
+
+app.post('/register-student', (req, res)=>{
+  console.log("Petición recibida");
+  console.log("Body del formulario: ", req.body);
+  // Actualizar la base de datos con el nuevo estudiante TODO
+  res.send("Hemos recibido tu solicitud");
+});
+
+/** Ejemplo ruta dinámica */
+/** Vamos a crear una ruta, que dado el dni de un estudiante, nos de información sobre el registro de dicho estudiante
+ * 
+ * /student/:dnistudent
+ */
+app.get('/student/:dnistudent', (req, res)=>{
+  console.log("Petición recibido");
+  // TODO: Emular que vamos a la base de datos y traemos info sobre el estudiante identificado con dnistudent
+  res.send(`Voy a traer información sobre el estudiante con dni ${req.params.dnistudent}`);
+});
+
 
 app.get('/about', (req, res)=> {
  const filePath = path.join(__dirname, 'views', 'about.html');
